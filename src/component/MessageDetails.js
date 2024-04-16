f920d561f1dc2feda3eff4b2bd7214bcce5f40d7
amitark7 <kumaramit21470@gmail.com>
2024-04-16 13:28:27 +0530

    create jsonData outside of folder and find data through params

--- src/component/MessageDetails.js
+++ src/component/MessageDetails.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
+import { messages } from "../JsonData/messages";
 
 const MessageDetails = () => {
-  const { state } = useLocation();
+  const { id } = useParams();
+  const messageData = messages.find((msg) => msg.id === Number(id));
   return (
     <div className="h-screen text-black p-10">
       <h1 className="text-center text-3xl font-semibold">
-        {state.message.title}
+        {messageData.title}
       </h1>
-      <p className="text-center mt-6 text-xl">{state.message.messages}</p>
+      <p className="text-center mt-6 text-xl">{messageData.messages}</p>
     </div>
   );
 };
--- src/component/Messages.js
+++ src/component/Messages.js
@@ -1,21 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { messages } from "../JsonData/messages";
 
 const Messages = () => {
-  const messages = [
-    {
-      id: 1,
-      title: "Messages 1",
-      messages:
-        "You Want to learn React Router then You must read React-Router Documentation",
-    },
-    { id: 2, title: "Messages 2", messages: "Hey User How are You ?" },
-    {
-      id: 3,
-      title: "Messages 3",
-      messages: "Thanks for Visit this site. Visit Again",
-    },
-  ];
   return (
     <div className="bg-blue-400 text-center h-screen text-white p-10">
       {messages.map((msg) => {
@@ -28,7 +15,6 @@ const Messages = () => {
               to={{
                 pathname: `/messages/${msg.id}`,
               }}
-              state={{ message: msg }}
               className="text-base sm:text-lg"
             >
               {msg.title}
